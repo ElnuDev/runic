a354003f3d1c5faf37c5b76fc3a959f2a7c45983
ElnuDev <elnu@elnu.com>
2022-04-14 11:43:09 -0700

    Improve renderer code structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use gtk::prelude::*;
 use gtk::glib;
-use gtk::pango;
+
+mod markdown;
+use markdown::{Renderer, Tag};
 
 fn build_ui(application: &gtk::Application) {
     let glade_src = include_str!("runic.glade");
@@ -14,34 +16,13 @@ fn build_ui(application: &gtk::Application) {
     let text_view: gtk::TextView = builder.object("text_view").unwrap();
     let buffer = text_view.buffer().unwrap();
 
-    let italics_text_tag = gtk::TextTag::new(Some("bold"));
-    italics_text_tag.set_property("style", pango::Style::Italic);
-    buffer.tag_table().unwrap().add(&italics_text_tag);
+    Tag::init_tags(&buffer);
 
     buffer.connect_changed(glib::clone!(@weak buffer => move |_| {
-        let text = {
-            let (start, end) = buffer.bounds();
-            buffer.remove_all_tags(&start, &end);
-            String::from(buffer.text(&start, &end, false).unwrap().as_str())
-        };
-        let mut in_italics = false;
-        let mut start = buffer.iter_at_offset(0);
-        let mut end;
-        for (i, char) in text.chars().enumerate() {
-            let i = i as i32;
-            if char == '*' {
-                if in_italics {
-                    end = buffer.iter_at_offset(i);
-                    buffer.apply_tag(&italics_text_tag, &start, &end);
-                } else {
-                    start = buffer.iter_at_offset(i);
-                }
-                in_italics = !in_italics;
-            }
-        }
+        let (start, end) = buffer.bounds();
+        Renderer::from(buffer.text(&start, &end, true).unwrap().as_str()).display(&buffer);
     }));
 
-    // TODO: What does glib::clone! and @weak do, exactly?
     button.connect_clicked(move |_| {
         buffer.insert_at_cursor("ඞ");
     });
